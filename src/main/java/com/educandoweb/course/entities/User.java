39a9adb39d376d1aadc538a2b37cd639305165dc
Luiz <luizroberto3268@gmail.com>
2025-02-12 08:38:30 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,7 +26,7 @@ public class User implements Serializable{
 	private String phone;
 	private String password;
 
-	@OneToMany(mappedBy = "client")
+	@OneToMany()
 	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
